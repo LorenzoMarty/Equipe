fe4892ff352eca2f602b5d74f329a12f9a82bf5b
lorenzo <lorenzo@gmail.com>
2024-03-11 16:57:40 -0300

    crud

--- editar.php
+++ editar.php
@@ -5,7 +5,7 @@ $idP = $_GET['id_produto'];
 
 include("conecta.php");
 
-$sql = "UPDATE produto SET nome = '$nome', qtd = '$qtd' WHERE id_produto= $idP";
+$sql = "UPDATE produto SET nome = '$nome', qtd = '$qtd' WHERE id_produto= '$idP'";
 
 if (mysqli_query($conexao, $sql)) {
     echo "Arquivo enviado com sucesso!";
